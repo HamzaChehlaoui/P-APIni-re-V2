1dd677d2e80273fc71c9ad9f989963e3c3c2a4b3
Hamza <hamzacoude@gmail.com>
2025-04-11 23:52:33 +0000

    Fix import path for Home component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './Home'; 
+import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import PlantsList from './pages/PlantsList';
